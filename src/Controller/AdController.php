b23ad1a9668402540d58c6520d287c28a9845eb0
Nono <Admin@MacBook-Pro-de-Test.local>
2020-11-10 11:50:20 +0100

    show ad et new ad

---
+++ src/Controller/AdController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo): Response
+    {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
+
+       //dump($ads);
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+    /**
+     * Permet de créer une annonce
+     * @Route("/ads/new", name="ads_create")
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return $this->render('ad/new.html.twig',[
+            
+        ]);
+    }
+
+    
+    /**
+     *  Permet d'afficher une seule annonce
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @param[string] $slug
+     * @return Response
+     */
+    public function show($slug, Ad $ad)
+    {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+        //$ad = $repo->findOneBySlug($slug);
+
+        
+        return $this->render('ad/show.html.twig',[
+            'ad' =>$ad
+        ]);
+    }
+
+    
+}
