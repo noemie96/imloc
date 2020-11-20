f8dc4e162f1217a2bd80d26e9b85d3292af2e2ab
Nono <Admin@MacBook-Pro-de-Test.local>
2020-11-20 14:48:49 +0100

    user inscription + header maj

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/login", name="account_login")
+     */
+    public function index(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * Permet de se déconnecter
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout(){
+        // besoin de rien d'autre
+    }
+
+    
+    /**
+     * Permet d'afficher le formulaire d'inscription et d'inscrire un utilisateur dans le site
+     * @Route("/register", name="account_reigster")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //crypter le mot de passe avec l'encoder
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash); //modifie le password
+
+            $manager->persist($user); //crée moi le sql pour toute les incertions dans la bdd
+            $manager->flush(); //envois à la base de donnée
+
+            $this->addFlash(
+                'success',
+                'Votre compte à bien été créer'
+            );
+
+            return $this->redirectToRoute('account_login');
+        }
+        return $this->render("account/registration.html.twig",[
+            'myForm'=>$form->createView()
+        ]);
+    }
+
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,49 +5,85 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Faker\Factory;
 //use Cocur\Slugify\Slugify;
+use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    // gestion du hash de password
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
         //$slugify = new Slugify();
 
+        // gestion des utilisateurs 
+        $users = []; // initialisation d'un tableau pour associer Ad et User
+        $genres = ['male','femelle'];
+
+        for($u=1; $u <= 10; $u++){ //compteur du for php classique boucle tant que a qui à commencé à 1 je continue à bouclé 
+            $user = new User();
+            $genre = $faker->randomElement($genres);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1,99).'.jpg';
+            $picture .= ($genre == 'male' ? 'men/' : 'women/').$pictureId;
+
+            $hash = $this->encoder->encodePassword($user,'password');
+
+            $user->setFirstName($faker->firstName($genre))
+                ->setLastName($faker->lastName())
+                ->setEmail($faker->email())
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'.join('</p><p>', $faker->paragraphs(3)).'</p>')
+                ->setPassword($hash)
+                ->setPicture($picture);
+
+            $manager->persist($user);
+            $users[]= $user; // ajouter l'utilisateur fraichement créé dans le tableau pour l'association avec les annonces    
+
+        }
+        //gestions des annonces
         for($a = 1; $a <= 30; $a++){
             $ad = new Ad();
             $title = $faker->sentence();
-            //$slug =$slugify->slugify($title);
+            //$slug = $slugify->slugify($title);
             //$coverImage = $faker->imageUrl(1000,350);
             $introduction = $faker->paragraph(2);
             $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
+            $user = $users[rand(0,count($users)-1)];
 
             $ad->setTitle($title)
                 ->setCoverImage('https://picsum.photos/1000/350')
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(rand(40,200))
-                ->setRooms(rand(1,5));
+                ->setRooms(rand(1,5))
+                ->setAuthor($user)
+                ;
 
-            $manager->persist($ad);
-
-            for($i=1; $i <=rand(2,5); $i++){
+            $manager->persist($ad); 
+            
+            for($i=1; $i <= rand(2,5); $i++){
                 $image = new Image();
                 $image->setUrl('https://picsum.photos/200/200')
                     ->setCaption($faker->sentence())
                     ->setAd($ad);
-                $manager->persist($image);
+                $manager->persist($image);    
             }
             
+
+
         }
-
-
-        // $product = new Product();
-        // $manager->persist($product);
-
         $manager->flush();
     }
 }
